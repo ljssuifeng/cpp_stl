5a2b78cc37f90a69b77eff1ea99a96fedf5cd08b
ljs <946459289@qq.com>
2020-10-04 22:57:05 +0800

    add: stacks

---
+++ structure_algorithms/stack/stack.h
@@ -0,0 +1,177 @@
+#pragma once
+#include <iostream>
+#include "structure_algorithms/linked_list/array_list.cc"
+#include "structure_algorithms/linked_list/array_list.h"
+#include "structure_algorithms/linked_list/chain.cc"
+#include "structure_algorithms/linked_list/chain.h"
+
+template <typename T>
+void changeLength1D(T*& source, int oldLength, int newLength) {
+  if (newLength < 0) {
+    throw std::invalid_argument("newLength must be > 0!");
+  }
+  T* temp = new T[newLength];
+  int number = std::min(oldLength, newLength);
+  std::copy(source, source + number, temp);
+  delete[] source;
+  source = temp;
+}
+
+// abstract class for stack
+template <typename T>
+class Stack {
+ public:
+  virtual ~Stack() {}
+  virtual bool empty() const = 0;
+  virtual int size() const = 0;
+  virtual T& top() = 0;
+  virtual void pop() = 0;
+  virtual void push(const T& theElement) = 0;
+};
+
+template <typename T>
+class DerivedArrayStack : private ArrayList<T>, public Stack<T> {
+ public:
+  DerivedArrayStack(int initialCapacity = 10) : ArrayList<T>(initialCapacity) {}
+  bool empty() const { return ArrayList<T>::empty(); }
+  int size() const { return ArrayList<T>::size(); }
+  T& top() {
+    try {
+      return ArrayList<T>::get(size() - 1);
+    } catch (std::invalid_argument) {
+      throw std::invalid_argument("stack empty!");
+    }
+  }
+
+  void pop() {
+    try {
+      ArrayList<T>::erase(size() - 1);
+    } catch (std::invalid_argument) {
+      throw std::invalid_argument("stack empty!");
+    }
+  }
+
+  void push(const T& theElement) { ArrayList<T>::insert(size(), theElement); }
+};
+
+// no derived class has better effciency
+template <typename T>
+class ArrayStack : public Stack<T> {
+ public:
+  ArrayStack(int initialCapacity = 10);
+  ~ArrayStack() { delete[] _stack; }
+  bool empty() const { return _stackTop == -1; }
+  int size() const { return _stackTop + 1; }
+  T& top() {
+    if (_stackTop == -1) {
+      throw std::invalid_argument("stack empty!");
+    }
+    return _stack[_stackTop];
+  }
+  void pop() {
+    if (_stackTop == -1) {
+      throw std::invalid_argument("stack empty!");
+    }
+    _stack[_stackTop--].~T();
+  }
+  void push(const T& theElement);
+
+ private:
+  T* _stack;
+  int _stackTop;
+  int _arrayLength;
+};
+
+template <typename T>
+ArrayStack<T>::ArrayStack(int initialCapacity) {
+  if (initialCapacity < 1) {
+    std::ostringstream s;
+    s << "Initial capacity = " << initialCapacity << " must be > 0";
+    throw std::invalid_argument(s.str());
+  }
+  _arrayLength = initialCapacity;
+  _stack = new T[_arrayLength];
+  _stackTop = -1;
+}
+
+template <typename T>
+void ArrayStack<T>::push(const T& theElement) {
+  if (_stackTop == _arrayLength - 1) {  // stack is full
+    changeLength1D(_stack, _arrayLength, _arrayLength * 2);
+    _arrayLength *= 2;
+  }
+  _stack[++_stackTop] = theElement;
+}
+
+template <typename T>
+class DerivedLinkedStack : private Chain<T>, public Stack<T> {
+ public:
+  DerivedLinkedStack(int initialCapacity = 10) : Chain<T>(initialCapacity) {}
+  bool empty() const { return Chain<T>::empty(); }
+  int size() const { return Chain<T>::size(); }
+  T& top() {
+    try {
+      return Chain<T>::get(size() - 1);
+    } catch (std::invalid_argument) {
+      throw std::invalid_argument("stack empty!");
+    }
+  }
+
+  void pop() {
+    try {
+      Chain<T>::erase(size() - 1);
+    } catch (std::invalid_argument) {
+      throw std::invalid_argument("stack empty!");
+    }
+  }
+
+  void push(const T& theElement) { Chain<T>::insert(size(), theElement); }
+};
+
+template <typename T>
+class LinkedStack : public Stack<T> {
+ public:
+  LinkedStack(int initialCapacity = 10) {
+    _stackTop = nullptr;
+    _stackSize = 0;
+  }
+  ~LinkedStack();
+  bool empty() const { return _stackSize == 0; }
+  int size() const { return _stackSize; }
+  T& top() {
+    if (_stackSize == 0) {
+      throw std::invalid_argument("stack is empty!");
+    }
+    return _stackTop->_element;
+  }
+  void pop();
+  void push(const T& theElement) {
+    _stackTop = new ChainNode<T>(theElement, _stackTop);
+    _stackSize++;
+  }
+
+ private:
+  ChainNode<T>* _stackTop;
+  int _stackSize;
+};
+
+template <typename T>
+LinkedStack<T>::~LinkedStack() {
+  while (_stackTop) {
+    ChainNode<T>* nextNode = _stackTop->_next;
+    delete _stackTop;
+    _stackTop = nextNode;
+    _stackSize--;
+  }
+}
+
+template <typename T>
+void LinkedStack<T>::pop() {
+  if (_stackSize == 0) {
+    throw std::invalid_argument("stack is empty!");
+  }
+  ChainNode<T>* nextNode = _stackTop->_next;
+  delete _stackTop;
+  _stackTop = nextNode;
+  _stackSize--;
+}
---
+++ structure_algorithms/stack/stack_test.cc
@@ -0,0 +1,143 @@
+#include "stack.h"
+/****array stack is faster than linked stack!!! ********/
+/****array stack and linked stack is faster than derived stack***********/
+
+// rail road cars
+ArrayStack<int>* track;
+int numberOfCars;
+int numberOfTracks;
+int smallestCar;
+int itsTrack;  // smallest car in which track
+
+bool putInHoldingTrack(int c) {
+  int bestTrack = 0;
+  int bestTop = numberOfCars + 1;
+
+  for (int i = 1; i <= numberOfTracks; ++i) {
+    if (!track[i].empty()) {
+      int topCar = track[i].top();
+      if (c < topCar && topCar < bestTop) {
+        bestTop = topCar;
+        bestTrack = i;
+      }
+    } else {
+      if (bestTrack == 0) {
+        bestTrack = i;
+      }
+    }
+  }
+  if (bestTrack == 0) {
+    return false;
+  }
+  track[bestTrack].push(c);
+  std::cout << "Move car " << c << " from input track to holding track "
+            << bestTrack << std::endl;
+  if (c < smallestCar) {
+    smallestCar = c;
+    itsTrack = bestTrack;
+  }
+  return true;
+}
+
+void outputFromHoldingTrack() {
+  track[itsTrack].pop();
+  std::cout << "Move car " << smallestCar << " from holding track " << itsTrack
+            << " to output track" << std::endl;
+  smallestCar = numberOfCars + 2;
+  for (int i = 1; i <= numberOfTracks; ++i) {
+    if (!track[i].empty() && (track[i].top() < smallestCar)) {
+      smallestCar = track[i].top();
+      itsTrack = i;
+    }
+  }
+}
+
+bool railRoad(int inputOrder[], int theNumberOfCars, int theNumberOfTracks) {
+  numberOfCars = theNumberOfCars;
+  numberOfTracks = theNumberOfTracks;
+
+  track = new ArrayStack<int>[theNumberOfTracks + 1];
+
+  int nextCarToOutput = 1;
+  smallestCar = theNumberOfCars + 1;
+
+  for (int i = 1; i <= theNumberOfCars; ++i) {
+    if (inputOrder[i] == nextCarToOutput) {
+      std::cout << "Move car " << inputOrder[i]
+                << " from input track to output track" << std::endl;
+      nextCarToOutput++;
+      while (smallestCar == nextCarToOutput) {
+        outputFromHoldingTrack();
+        nextCarToOutput++;
+      }
+    } else {
+      if (!putInHoldingTrack(inputOrder[i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+int main() {
+  using std::cout;
+  using std::endl;
+  DerivedArrayStack<double> das(10);
+  cout << "is das empty? " << std::boolalpha << das.empty() << endl;
+
+  das.push(12);
+  das.push(23);
+  das.push(34);
+  cout << "das size:" << das.size() << endl;
+  cout << "top element of das is: " << das.top() << endl;
+  das.pop();
+  cout << "size of das after pop is:" << das.size() << endl;
+  das.pop();
+  das.pop();
+  //  das.pop();   // will throw exception
+
+  ArrayStack<int> as(2);
+  as.push(2);
+  as.push(22);
+  as.push(222);
+
+  cout << "as size: " << as.size() << endl;
+  cout << "as is empty? " << std::boolalpha << as.empty() << endl;
+  cout << "as top element is: " << as.top() << endl;
+  as.pop();
+  cout << "as size after pop: " << as.size() << endl;
+  as.pop();
+  as.pop();
+  //  as.pop(); //throw exception here   stack empty
+
+  DerivedLinkedStack<double> dls(10);
+  cout << "is dls empty? " << std::boolalpha << dls.empty() << endl;
+
+  dls.push(12);
+  dls.push(23);
+  dls.push(34);
+  cout << "dls size:" << dls.size() << endl;
+  cout << "top element of dls is: " << dls.top() << endl;
+  dls.pop();
+  cout << "size of dls after pop:" << dls.size() << endl;
+  dls.pop();
+  dls.pop();
+  //  dls.pop();  // will throw exception
+
+  LinkedStack<int> ls(2);
+  ls.push(2);
+  ls.push(22);
+  ls.push(222);
+
+  cout << "ls size: " << ls.size() << endl;
+  cout << "ls is empty? " << std::boolalpha << ls.empty() << endl;
+  cout << "ls top element is: " << ls.top() << endl;
+  ls.pop();
+  cout << "ls size after pop: " << ls.size() << endl;
+  ls.pop();
+  ls.pop();
+  //  ls.pop();  // throw exception here   stack empty
+
+  int carOrder[]{0, 3, 6, 9, 4, 8, 1, 2, 7, 10, 5};
+  railRoad(carOrder, 10, 3);
+}
